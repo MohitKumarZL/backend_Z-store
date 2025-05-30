b86c8e0555c4d1bfd736e1dcbf3ff111f9673cba
Mohit Kumar <mohitkumarzl@hotmail.com>
2025-05-30 18:45:55 +0530

    new update

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const http = require('http')
 
 
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
   // console.log(req.url, req.headers , req.method)
-  if(req.url === '/'){
-console.log('Welcome')  }else{  return res.end()
-}
+  if (req.url === '/') {
+    console.log('Welcome')
+  } else {
+    return res.end()
+  }
 }
 
 )
-const PORT =3000
+const PORT = 3000
 
-server.listen(PORT, ()=> console.log(`Server at http://localhost:${PORT}`))
+server.listen(PORT, () => console.log(`Server at http://localhost:${PORT}`))
