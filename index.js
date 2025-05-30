f244645c6a25e6c8617bb48eb34541012b6b6588
Mohit Kumar <mohitkumarzl@hotmail.com>
2025-05-30 17:52:24 +0530

    All requirement added

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello')
+const express = require('express')
+const http = require('http')
+
+
+
+
+
+
+const server = http.createServer((req,res)=>{
+  // console.log(req.url, req.headers , req.method)
+  if(req.url === '/'){
+console.log('Welcome')  }else{  return res.end()
+}
+}
+
+)
+const PORT =3000
+
+server.listen(PORT, ()=> console.log(`Server at http://localhost:${PORT}`))
