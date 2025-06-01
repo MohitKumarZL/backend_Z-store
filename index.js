ae0f240f52e6d8ef1c111cc379ee3f05c887f213
Mohit Kumar <mohitkumarzl@hotmail.com>
2025-06-01 18:47:45 +0530

    first controller created

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const express = require('express')
-const http = require('http')
+const app=express();
+
+
+
+app.get("/",(req, res, next)=>{
+    console.log("Register 1")
+
+  res.send(`<h2>Welcome in new app</h2>
+    <a href="/add-home">go to home</a>`
+  )
+})
+
+app.get("/add-home",(req,res,next)=>{
+  console.log("Register")
+  res.send(`
+    <h2> Welcome to Register</h2>
+    <form action="/add-home" method="POST">
+    <input type="text" placeholder="Enter your name"/>
+    <input type="submit"/>
+    </form>
+    `)
+})
+
+
+app.post("/add-home",(req,res,next)=>{
+  console.log("Register")
+  res.send(`
+    <h2> Registered Display</h2>
+    `)
+})
 
 
 
 
-
-
-const server = http.createServer((req, res) => {
-  // console.log(req.url, req.headers , req.method)
-  if (req.url === '/') {
-    console.log('Welcome')
-  } else {
-    return res.end()
-  }
-}
-
-)
 const PORT = 3000
-
-server.listen(PORT, () => console.log(`Server at http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Server at http://localhost:${PORT}`))
